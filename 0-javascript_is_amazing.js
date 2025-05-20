fd4155b18c1d5e16872e91727c134f1d37126709
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-05-20 10:22:03 +0000

    Update 0-javascript_is_amazing.js

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,8 +1,10 @@
-// Define a constant variable
+// Declare a constant variable
 const myVar = "JavaScript is amazing";
 
-// Use a loop to print the output
+// Initialize a counter
 let count = 0;
+
+// Use a while loop to print the message 3 times
 while (count < 3) {
     console.log(myVar);
     count++;
